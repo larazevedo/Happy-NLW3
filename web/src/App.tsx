8fe4a23bafec7496273dc1b4fd5c62924f977ad4
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-12 13:16:13 -0300

    Adicionando logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,8 +9,11 @@ function App() {
   return (
     <div id="page-landing">
      <div className="content-wrapper">
-        <img src={logoImg} alt="" />
-
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças </p>
+        </main>
      </div>
     </div>
   );
