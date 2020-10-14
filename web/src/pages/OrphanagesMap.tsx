a59a97b095a296850eb4ba2209ec255b8cad040e
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-14 01:04:39 -0300

    Página do Map estilizada :)

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-maker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>Oi</h1>
+       <div id="page-map">
+           <aside>
+               <header>
+                   <img src={mapMarkerImg} alt="Happy"/>
+
+                   <h2>Escolha um orfanato no mapa</h2>
+                   <p>Muitas crianças estão esperando a sua visita :)</p>
+               </header>
+
+               <footer>
+                   <strong>São João da Boa Vista</strong>
+                   <span>São Paulo</span>
+               </footer>
+           </aside>
+
+           <div></div>
+
+        <Link to="" className="create-orphanage">
+            <FiPlus size={32} color="#fff" />
+        </Link>
+
+       </div>
     )
 }
 
