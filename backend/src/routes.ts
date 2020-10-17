b3a6951d1d0905442db785e8e2cb8660819e294b
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-17 11:43:31 -0300

    Detalhe do orfanato - método show

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesCotroller from './controllers/OrphanagesController'
 const routes = Router();
 
 routes.get('/orphanages', OrphanagesCotroller.index);
+routes.get('/orphanages/:id', OrphanagesCotroller.show);
 routes.post('/orphanages', OrphanagesCotroller.create);   
 
 export default routes;
