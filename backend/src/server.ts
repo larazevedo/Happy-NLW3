3d7dff470944e932e48821bfba314a7dbd6d1251
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-17 13:52:18 -0300

    Lidando com exceções (express async errors)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
 import './database/connection';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -9,4 +11,5 @@ app.use(express.json());
 
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 app.listen(3333);
