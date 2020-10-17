70f8eab5a7ded2e900f6898a7b81a9410f5c4c38
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-17 11:23:34 -0300

    Listando orfanatos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import OrphanagesCotroller from './controllers/OrphanagesController'
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesCotroller.index);
 routes.post('/orphanages', OrphanagesCotroller.create);   
 
 export default routes;
