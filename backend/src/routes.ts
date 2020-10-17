1826ce86365df61e2f81f70cfe5210f2a77436dc
Lara Azevedo <lara.azevedo@outlook.com>
2020-10-17 13:29:17 -0300

    Upload de imagens funcionando :)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesCotroller from './controllers/OrphanagesController'
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesCotroller.index);
 routes.get('/orphanages/:id', OrphanagesCotroller.show);
-routes.post('/orphanages', OrphanagesCotroller.create);   
+routes.post('/orphanages', upload.array('images'), OrphanagesCotroller.create);   
 
 export default routes;
